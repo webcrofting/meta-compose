1c65f00f04c18b18acb9ac8827dde0771c198cf7
Christoph Witzany <christoph@web.crofting.com>
2015-06-10 09:40:15 +0200

    fix error when no meta-compose-data.yml present
    Fixes issue #1

--- metacompose/cli/main.py
+++ metacompose/cli/main.py
@@ -7,13 +7,21 @@ def env_override(value, key):
   return os.getenv(key, value)
 
 def main():
+
+    template_file_name = "meta-compose.yml"
+    data_file_name = "meta-compose-data.yml"
+
     jinja = Environment(loader=FileSystemLoader("."))
     jinja.filters['env'] = env_override
 
-    template = jinja.get_template("meta-compose.yml")
+    template = jinja.get_template(template_file_name)
 
-    with open("meta-compose-data.yml", "r") as fh:
-        data = yaml.safe_load(fh)
+
+    if os.path.isfile(data_file_name):
+        with open(data_file_name, "r") as fh:
+            data = yaml.safe_load(fh)
+    else:
+        data = {}
 
     composition = template.render(data)
 
